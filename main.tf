b6c15b0a873861b13019a3f1b0872a1a73879a56
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2022-01-26 21:52:55 -0800

    Add Random Suffix

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
+resource "random_string" "suffix" {
+  length  = 4
+  special = false
+  lower   = true
+  upper   = false
+}
+
 resource "azurerm_resource_group" "example" {
-  name     = var.NAME
+  name     = "${var.NAME}-${random_string.suffix.result}" 
   location = "EastAsia"
 }
