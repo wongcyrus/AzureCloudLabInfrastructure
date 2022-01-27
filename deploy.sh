afc75a24fc50fc538da3f39bc795cb198483c860
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2022-01-27 00:40:44 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,4 @@ git clone https://github.com/wongcyrus/AzureCloudLabInfrastructure
 cd AzureCloudLabInfrastructure
 terraform init
 terraform apply -auto-approve
+terraform output -json > output.json
