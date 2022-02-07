2a4c84c0222e18e28a9192a16a7240b90ed17390
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2022-02-07 01:07:45 -0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,18 @@
+output "EMAIL" {
+  value       = var.EMAIL
+  description = "Student Email"
+}
+
+output "LAB" {
+  value       = var.LAB
+  description = "LAB"
+}
+
+
 output "resource_group_name" {
   value       = azurerm_resource_group.example.name
   description = "Resource Group Name"
 }
+
+
+
