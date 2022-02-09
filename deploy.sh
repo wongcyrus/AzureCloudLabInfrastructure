11794a39baa1765d4b0aa4c2bcbcad6808d410e0
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2022-02-08 23:07:27 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,9 @@ cd ${LAB}/${EMAIL}
 rm -rf AzureCloudLabInfrastructure/
 git clone https://github.com/wongcyrus/AzureCloudLabInfrastructure
 cd AzureCloudLabInfrastructure
+git checkout ${BRANCH}
 terraform init
 terraform apply -auto-approve
 terraform output -json > output.json
 cat output.json
+curl -d @output.json ${CALLBACK_URL}
