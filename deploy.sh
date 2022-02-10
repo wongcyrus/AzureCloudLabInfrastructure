342c42be3daf22554e539c2e0a8bb891db259815
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2022-02-09 19:29:49 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+az login --service-principal -u ${ARM_CLIENT_ID} -p ${ARM_CLIENT_SECRET} --tenant ${ARM_TENANT_ID}
 mkdir -p ${LAB}/${EMAIL}
 cd ${LAB}/${EMAIL}
 rm -rf AzureCloudLabInfrastructure/
