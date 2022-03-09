fce5c096af5c8bf3255a21f1718dc3b7f0927b38
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2022-03-08 23:01:13 -0800

    Update undeploy.sh

--- undeploy.sh
+++ undeploy.sh
@@ -2,8 +2,6 @@
 az login --service-principal -u ${ARM_CLIENT_ID} -p ${ARM_CLIENT_SECRET} --tenant ${ARM_TENANT_ID}
 cd ${LAB}/${EMAIL}/AzureCloudLabInfrastructure
 ls
-mkdir -p $HOME/.terraform.d/plugin-cache
-export TF_PLUGIN_CACHE_DIR="$HOME/.terraform.d/plugin-cache"
 terraform init
 terraform refresh
 terraform destroy -auto-approve
