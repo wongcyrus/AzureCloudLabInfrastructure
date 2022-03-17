ce430a2ab2787b21fba079c53f48402c60b61086
Cyrus Wong <cywong@vtc.edu.hk>
2022-03-17 17:08:08 +0800

    Add ACR settings in variables.

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "random_string" "suffix" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.LAB}-${lower(replace(var.EMAIL,"/\\W|_|\\s/","-"))}"
+  name     = "${var.LAB}-${lower(replace(var.EMAIL, "/\\W|_|\\s/", "-"))}"
   location = "EastAsia"
   tags = {
     email = "${var.EMAIL}"
@@ -19,50 +19,7 @@ resource "random_password" "login_password" {
   override_special = "!@#$%&"
 }
 
-resource "azurerm_container_registry" "acr" {
-  name                = "${random_string.suffix.result}BastionContainerRegistry"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku                 = "Standard"
-  admin_enabled       = true
-}
-
-resource "azurerm_container_registry_task" "build_bastion_image_task" {
-  name                  = "build_bastion_image_task"
-  container_registry_id = azurerm_container_registry.acr.id
-  platform {
-    os = "Linux"
-  }
-  docker_step {
-    dockerfile_path      = "Dockerfile"
-    context_path         = "https://github.com/wongcyrus/ssh-tunneling-bastion#main"
-    context_access_token = "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"
-    image_names          = ["ssh-tunneling-bastion:latest"]
-    arguments = {
-      AZURE_CLI_VERSION = "2.32.0"
-      TERRAFORM_VERSION = "1.1.4"
-    }
-  }
-}
-
-data "http" "docker_file" {
-  url = "https://raw.githubusercontent.com/wongcyrus/ssh-tunneling-bastion/master/Dockerfile"
-}
-
-resource "null_resource" "run_arc_task" {
-  provisioner "local-exec" {
-    command = "az acr task run --registry ${azurerm_container_registry.acr.name} --name build_bastion_image_task"
-  }
-  depends_on = [azurerm_container_registry_task.build_bastion_image_task]
-  triggers = {
-    dockerfile = data.http.docker_file.body
-  }
-}
-
 resource "azurerm_container_group" "bastion" {
-  depends_on = [
-    null_resource.run_arc_task
-  ]
   name                = "${var.LAB}-${random_string.suffix.result}-bastion"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -70,13 +27,13 @@ resource "azurerm_container_group" "bastion" {
   os_type             = "Linux"
 
   image_registry_credential {
-    username = azurerm_container_registry.acr.admin_username
-    password = azurerm_container_registry.acr.admin_password
-    server   = azurerm_container_registry.acr.login_server
+    username = var.BASTION_ARC_ADMIN_USERNAME
+    password = var.BASTION_ARC_ADMIN_PASSWORD
+    server   = var.BASTION_ARC_LOGIN_SERVER
   }
   container {
     name   = "ssh-tunneling-bastion"
-    image  = "${azurerm_container_registry.acr.login_server}/ssh-tunneling-bastion"
+    image  = "${var.BASTION_ARC_LOGIN_SERVER}/ssh-tunneling-bastion"
     cpu    = "1"
     memory = "1"
     environment_variables = {
--- variables.tf
+++ variables.tf
@@ -2,6 +2,10 @@ variable "LAB" {}
 variable "EMAIL" {}
 variable "LOCATION" {}
 variable "REPEAT_TIMES" {}
+variable "BASTION_ARC_ADMIN_USERNAME" {}
+variable "BASTION_ARC_ADMIN_PASSWORD" {}
+variable "BASTION_ARC_LOGIN_SERVER" {}
+
 # variable "SUBSCRIPTION_ID" {}
 # variable "TENANT_ID" {}
 
