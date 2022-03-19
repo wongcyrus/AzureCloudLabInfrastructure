80797d6fbccc719d90cae76f71f2b96da5579d12
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2022-03-19 06:40:17 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ az login --service-principal -u ${ARM_CLIENT_ID} -p ${ARM_CLIENT_SECRET} --tenan
 mkdir -p ${LAB}/${EMAIL}
 cd ${LAB}/${EMAIL}
 rm -rf AzureCloudLabInfrastructure/
-curl -L -k --retry 5 --retry-delay 0 -o code.zip https://github.com/wongcyrus/AzureCloudLabInfrastructure/archive/refs/heads/${BRANCH}.zip
+curl -L -k --retry-all-errors --retry 5 --retry-delay 0 -o code.zip https://github.com/wongcyrus/AzureCloudLabInfrastructure/archive/refs/heads/${BRANCH}.zip
 unzip code.zip
 rm code.zip
 mv AzureCloudLabInfrastructure-${BRANCH} AzureCloudLabInfrastructure
